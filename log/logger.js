7d6f843ff0a6f86452b35aa98f58d8528b4df726
Tyler Moore <mooretc2@miamioh.edu>
2018-03-28 13:30:40 -0400

    trying to fix logger

--- log/logger.js
+++ log/logger.js
@@ -9,7 +9,7 @@ function formatParams(info) {
   const ts = timestamp.slice(0, 19).replace("T", " ");
 
   return `${ts} ${level}: ${message} ${Object.keys(args).length
-    ? JSON.stringify(args, "", "")
+    ? args
     : ""}`;
 }
 
