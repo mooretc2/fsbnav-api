2a5b40a899d20f58d9876162bc09d1fb6b52f09c
gouldijt <gouldijt@miamioh.edu>
2018-04-22 20:54:59 -0400

    router function
    Programmed the router function, and two helper functions. there is a
    third helper function that still needs to be put in, and I am having
    difficulties with getting arrays and queues to provide their functions
    when passed to a helper.

--- api/controllers/routeController.js
+++ api/controllers/routeController.js
@@ -2,8 +2,66 @@ var logger = require('../../log/logger'),
     accessLog = logger.accessLog,
     errorLog = logger.errorLog;
 
-function router(begin, end, stairs){
-	//TODO: search algorithm
+//current algorithm does not make use of searching through database nodes, getting edges, or checking for stair/elevator preferences
+//origin and destination must be ints (primary key of node)
+//preference must be a tinyint (0 or 1)
+function router(origin, destination, preference){
+	
+    //number of nodes (must be pulled from database... and stored in web services globally)
+    var numOfNodes = 70;
+
+    //boolean arrays to store visited node primary keys
+    var originVisited = new Array(numOfNodes).fill(false);
+    var destVisited = new Array(numOfNodes).fill(false);
+
+    //int arrays to track node parents
+    var originParent = new Array(numOfNodes);
+    var destParent = new Array(numOfNodes);
+
+    //int queue for front and back searches
+    var originQueue = [];
+    var destQueue = [];
+
+    //track intersect node: must be an int to work properly
+    intersectNode = -1;
+
+    originQueue.push(origin);
+    originVisited[origin] = true;
+    originParent[origin] = -1;
+    
+    destQueue.push(destination);
+    destVisited[destination] = true;
+    destParent[destination] = -1;
+
+    while (originQueue.length != 0 && destQueue.length != 0) {
+        //BFS for both
+        BFS(originQueue, originVisited, originParent);
+        BFS(destQueue, destVisited, destParent);
+
+        //check for intersection
+        intersectNode = isIntersecting(numOfNodes, originVisited, destVisited);
+
+        //if intersecting vertex is found, there is a path
+        if (intersectNode != -1) {
+            //return path. still needs to be done
+        }
+    }
+    //Failure. not sure what to put here if failure occurs
+}
+
+//Breadth First Search function. used by router
+function BFS(queue, visited, parent) {
+    //BFS code... can't seem to get array or queue functions
+    
+}
+
+//check for intersection between BFS. used by router
+function isIntersecting(numOfNodes, originVisited, destVisited) {
+    for (i = 0; i < numOfNodes; i++) {
+        if (originVisited[i] && destVisited[i]) //I don't get errors with this line, so why does this work?
+            return i;
+    }
+    return -1;
 }
 
 exports.getRoute = function(req, res){
