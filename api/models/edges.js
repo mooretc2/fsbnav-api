01d8fc4843518965065701900e6309371fe71400
Tyler Moore <mooretc2@miamioh.edu>
2018-04-29 23:21:01 -0400

    getAdjacentNode finished

--- api/models/edges.js
+++ api/models/edges.js
@@ -13,3 +13,16 @@ exports.getNumEdges = function (done) {
         done(null, rows);
     });
 }
+
+exports.getAdjacentNode = function(nodeID, edgeID, stairs, done) {
+    db.get().query('SELECT node1ID, node2ID, transition FROM edge WHERE edgeID = ?', edgeID, function (err, rows) {
+        if (err) return done(err);
+        if (rows[0].transition != 'NULL' && rows[0].transition != stairs){
+            done(null, null);
+        } else if (rows[0].node1ID == nodeID){
+            done(null, rows[0].node2ID);
+        } else if (rows[0].node2ID == nodeID){
+            done(null, rows[0].node1ID);
+        }
+    });
+}
