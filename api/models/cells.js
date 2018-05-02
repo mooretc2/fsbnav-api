686c400387f857d8ccb11a466ac5d9ecb1e014ed
Tyler Moore <mooretc2@miamioh.edu>
2018-05-01 22:12:05 -0400

    Cells model converted to use promises

--- api/models/cells.js
+++ api/models/cells.js
@@ -1,9 +1,11 @@
 var db = require('../../db.js');
 
-exports.getCellByNodeID = function (nodeID, done){
-    db.get().query('SELECT cell.floorID, cell.longitude, cell.latitude FROM cell '
-            + 'LEFT JOIN node ON cell.cellID = node.cellID WHERE node.nodeID = ?', nodeID, function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
-    });
+exports.getCellByNodeID = function (nodeID, done) {
+	return new Promise(function (resolve, reject) {
+		db.get().query('SELECT cell.floorID, cell.longitude, cell.latitude FROM cell '
+			+ 'LEFT JOIN node ON cell.cellID = node.cellID WHERE node.nodeID = ?', nodeID, function (err, rows) {
+				if (err) reject(err);
+				else resolve(rows);
+			});
+	});
 }
