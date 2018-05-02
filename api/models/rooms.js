404a6e69c262edaa21415fff7f71068fda7e687b
Tyler Moore <mooretc2@miamioh.edu>
2018-05-01 22:14:07 -0400

    Rooms model converted to use promises

--- api/models/rooms.js
+++ api/models/rooms.js
@@ -1,22 +1,28 @@
 var db = require('../../db.js');
 
 exports.getAll = function (done) {
-    db.get().query('SELECT roomName, isPopular FROM room', function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
+    return new Promise(function (resolve, reject) {
+        db.get().query('SELECT roomName, isPopular FROM room', function (err, rows) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
     });
 }
 
 exports.getAllStartingWith = function (roomID, done) {
-    db.get().query("SELECT roomName FROM room WHERE roomNumber LIKE ?", roomID+'%', function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
+    return new Promise(function (resolve, reject) {
+        db.get().query("SELECT roomName FROM room WHERE roomNumber LIKE ?", roomID + '%', function (err, rows) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
     });
 }
 
 exports.getNodeIDByRoomID = function (roomID, done) {
-    db.get().query("SELECT nodeID FROM room WHERE roomID = ?", roomID, function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
+    return new Promise(function (resolve, reject) {
+        db.get().query("SELECT nodeID FROM room WHERE roomID = ?", roomID, function (err, rows) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
     });
 }
