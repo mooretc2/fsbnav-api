d47484750070929a7ca93f1bbb3832d53fad14df
Tyler Moore <mooretc2@miamioh.edu>
2018-05-01 22:09:55 -0400

    Edges model converted to use promises

--- api/models/edges.js
+++ api/models/edges.js
@@ -1,29 +1,35 @@
 var db = require('../../db.js');
 
 exports.getAll = function (done) {
-    db.get().query('SELECT * FROM edge', function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
+    return new Promise(function (resolve, reject) {
+        db.get().query('SELECT * FROM edge', function (err, rows) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
     });
 }
 
 exports.getNumEdges = function (done) {
-    db.get().query('SELECT COUNT(edgeID) FROM edge', function (err, rows) {
-        if (err) return done(err);
-        done(null, rows);
+    return new Promise(function (resolve, reject) {
+        db.get().query('SELECT COUNT(edgeID) FROM edge', function (err, rows) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
     });
 }
 
-exports.getAdjacentNode = function(nodeID, stairs, done) {
-    db.get().query('(SELECT node1ID AS node FROM edge WHERE (node2ID = ? AND (transition = ? OR transition IS NULL))) UNION' +
-        ' (SELECT node2ID AS node FROM edge WHERE (node1ID = ? AND (transition = ? OR transition IS NULL)))', nodeID, stairs, function (err, rows) {
-            if (err) return done(err);
-            else {
-                var data;
-                for (x in rows){
-                    data.push(x.node);
+exports.getAdjacentNode = function (nodeID, stairs, done) {
+    return new Promise(function (resolve, reject) {
+        db.get().query('(SELECT node1ID AS node FROM edge WHERE (node2ID = ? AND (transition = ? OR transition IS NULL))) UNION' +
+            ' (SELECT node2ID AS node FROM edge WHERE (node1ID = ? AND (transition = ? OR transition IS NULL)))', nodeID, stairs, function (err, rows) {
+                if (err) reject(err);
+                else {
+                    var data;
+                    for (x in rows) {
+                        data.push(x.node);
+                    }
+                    resolve(data);
                 }
-                done(null, data);
-            }
+            });
     });
 }
